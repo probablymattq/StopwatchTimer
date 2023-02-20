c6988abdda2e551f649ad5f169e83418d4877b1f
probablymattq <ionq652@gmail.com>
2023-02-20 23:26:32 +0200

    Stopwatch started

--- app/src/main/java/com/matter/stopwatchtimer/MainActivity2.java
+++ app/src/main/java/com/matter/stopwatchtimer/MainActivity2.java
@@ -5,10 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+    private TextView mMinutesDisplay;
+    private TextView mSecondsDisplay;
+    private Button mStartPauseButton;
+    private Button mResetButton;
+
+    private CountDownTimer mCountDownTimer;
+    private boolean mTimerRunning;
+    private long mTimeElapsed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +26,18 @@ public class MainActivity2 extends AppCompatActivity {
         overridePendingTransition(0, 0);
         setContentView(R.layout.activity_main2);
 
+
+        mMinutesDisplay = findViewById(R.id.minutes_display);
+        mSecondsDisplay = findViewById(R.id.seconds_display);
+        mStartPauseButton = findViewById(R.id.start_stop_button);
+        mResetButton = findViewById(R.id.reset_button);
+
+        mMinutesDisplay.setText("00");
+        mSecondsDisplay.setText("00");
+
         TextView timerTextView = findViewById(R.id.timerView);
         TextView stopwatchTextView = findViewById(R.id.stopwatchView);
+
 
         timerTextView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,5 +48,65 @@ public class MainActivity2 extends AppCompatActivity {
         });
 
         stopwatchTextView.setPaintFlags(stopwatchTextView.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
+        mStartPauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mTimerRunning) {
+                    pauseTimer();
+                } else {
+                    startTimer();
+                }
+            }
+        });
+
+        mResetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetTimer();
+            }
+        });
+    }
+
+    private void startTimer() {
+        mCountDownTimer = new CountDownTimer(Long.MAX_VALUE, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeElapsed += 1000;
+                updateDisplay();
+            }
+
+            @Override
+            public void onFinish() {
+                // Do nothing
+            }
+        }.start();
+
+        mTimerRunning = true;
+        mStartPauseButton.setText("Pause");
+    }
+
+    private void pauseTimer() {
+        mCountDownTimer.cancel();
+        mTimerRunning = false;
+        mStartPauseButton.setText("Resume");
+    }
+
+    private void resetTimer() {
+        if(mTimerRunning || mTimeElapsed > 0) {
+            mCountDownTimer.cancel();
+            mTimeElapsed = 0;
+            updateDisplay();
+            mTimerRunning = false;
+            mStartPauseButton.setText("Start");
+        }
+    }
+
+    private void updateDisplay() {
+        int minutes = (int) (mTimeElapsed / 1000) / 60;
+        int seconds = (int) (mTimeElapsed / 1000) % 60;
+
+        mMinutesDisplay.setText(String.format("%02d", minutes));
+        mSecondsDisplay.setText(String.format("%02d", seconds));
     }
 }
